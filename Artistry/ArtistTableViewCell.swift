b6c1a5eccf8655e0d4deb6b8d38fc91bcf8b2b11
Sam Wei <sam@beokay.com>
2017-02-27 17:06:18 -0500

    init

---
+++ Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by WeiShengkun on 2/27/17.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+
+  
+  
+}
---
+++ Artistry/WorkTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by WeiShengkun on 2/27/17.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+  
+  
+  
+  
+  
+  
+}
